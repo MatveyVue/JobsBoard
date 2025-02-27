3918572943fdc4e087c3b819e557975d12180373
MatveyVue <shabanov.matveyka@bk.ru>
2025-02-27 15:04:00 -0800

    Add files via upload

---
+++ src/views/HomeView.vue
@@ -0,0 +1,90 @@
+<template>
+<body>
+<div>
+<RouterLink to="/profile">
+    <img src="https://i.postimg.cc/FK8K0bcd/IMG-1157.png" style="width: 40px; float: right; margin-right: 10px; margin-top: 10px;">
+</RouterLink>
+</div>
+
+<div>
+    <h1>Jobs:</h1>
+    <div style="overflow-x: scroll;">
+        <button class="categories" style="background-color: #97f492; color: black;">All</button>
+        <button class="categories">Programm</button>
+        <button class="categories">Ads</button>
+    </div>
+</div>
+
+<div>
+<button @click="open = true" class="jobs1">
+<div>
+    <img class="img" src="https://i.postimg.cc/FK8K0bcd/IMG-1157.png" style="width: 30px; float: left; margin-top: 10px; margin-left: 10px;">
+    <p class="nick">Whsxg</p>
+    <p class="work">Frontend Deveoloper</p>
+    <p class="info">Разработка Telegram Mini App</p><br>
+    <p class="info" style="margin-left: 8px; margin-top: -12px;">по задонному техническому заданию</p>
+</div>
+<div style="display: inline-flex; margin-top: -115px; float: left;">
+    <div class="lang"><p style="margin-top: 5px; margin-left: -3px;">JavaScript</p></div>
+    <div class="lang"><p style="margin-top: 5px; margin-left: -3px;">Rust</p></div>
+</div>
+</button>
+
+<div v-if="open" class="modal">
+<div style="margin-top: 20px; margin-left: 20px;">
+    <img class="img" src="https://i.postimg.cc/FK8K0bcd/IMG-1157.png" style="width: 30px; float: left; margin-top: 10px; margin-left: 10px;">
+    <p class="nick">Whsxg</p>
+    <p class="work">Frontend Deveoloper</p>
+    <p class="info">Разработка Telegram Mini App</p><br>
+    <p class="info" style="margin-left: 8px; margin-top: -12px;">по задонному техническому заданию</p>
+</div>
+<div style="display: inline-flex; margin-top: -110px; float: left; margin-left: 20px;">
+    <div class="lang"><p style="margin-top: 5px; margin-left: 10px;">JavaScript</p></div>
+    <div class="lang"><p style="margin-top: 5px; margin-left: 30px;">Rust</p></div>
+</div>
+    <button class="contact"><b>Contact</b></button>
+    <button @click="open = false" class="close">X</button>
+</div>
+</div>
+
+<div>
+<button @click="open1 = true" class="jobs1">
+<div>
+    <img class="img" src="https://i.postimg.cc/FK8K0bcd/IMG-1157.png" style="width: 30px; float: left; margin-top: 10px; margin-left: 10px;">
+    <p class="nick">Whsxg</p>
+    <p class="work">Frontend Deveoloper</p>
+    <p class="info">Разработка Telegram Mini App</p><br>
+    <p class="info" style="margin-left: 8px; margin-top: -12px;">по задонному техническому заданию</p>
+</div>
+<div style="display: inline-flex; margin-top: -115px; float: left;">
+    <div class="lang"><p style="margin-top: 5px; margin-left: -3px;">JavaScript</p></div>
+    <div class="lang"><p style="margin-top: 5px; margin-left: -3px;">Rust</p></div>
+</div>
+</button>
+
+<div v-if="open1" class="modal">
+<div style="margin-top: 20px; margin-left: 20px;">
+    <img class="img" src="https://i.postimg.cc/FK8K0bcd/IMG-1157.png" style="width: 30px; float: left; margin-top: 10px; margin-left: 10px;">
+    <p class="nick">Whsxg</p>
+    <p class="work">Frontend Deveoloper</p>
+    <p class="info">Разработка Telegram Mini App</p><br>
+    <p class="info" style="margin-left: 8px; margin-top: -12px;">по задонному техническому заданию</p>
+</div>
+<div style="display: inline-flex; margin-top: -110px; float: left; margin-left: 20px;">
+    <div class="lang"><p style="margin-top: 5px; margin-left: 10px;">JavaScript</p></div>
+    <div class="lang"><p style="margin-top: 5px; margin-left: 30px;">Rust</p></div>
+</div>
+    <button class="contact"><b>Contact</b></button>
+    <button @click="open1 = false" class="close">X</button>
+</div>
+</div>
+</body>
+</template>
+    
+<script setup>
+import { ref } from 'vue'
+
+const open = ref(false)
+const open1 = ref(false)
+</script>
+    
---
+++ src/views/ProfileView.vue
@@ -0,0 +1,24 @@
+<template>
+<body>
+<div>
+    <center>
+    <RouterLink to="/">
+        <img src="https://i.postimg.cc/PxR6j6Rc/BFF14-B15-FF7-A-41-A2-A7-AB-AC75-B7-DE5-FD7.png" width="50px" style="position: absolute; margin-left: -140px;">
+    </RouterLink>
+    <img src="https://i.postimg.cc/FK8K0bcd/IMG-1157.png" style="width: 120px; margin-top: 50px;">
+    <p style="font-family: Geologica; color: white; font-size: 24px;">@whsxg</p>
+    </center>
+</div>
+</body>
+</template>
+
+<style scoped>
+body {
+    background-color: #101622;
+    font-weight: normal;
+    overflow-x: hidden;
+} 
+a {
+    text-decoration: none;
+}
+</style>
